ec3310a60d590e223e0445dc580984cf0de1344e
Caio Orleans <caioorleansdm@gmail.com>
2025-06-18 16:22:47 -0300

    feat: icone de favoritos da topbar

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,18 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { IonToolbar, IonTitle } from '@ionic/angular/standalone';
+import { IonToolbar, IonTitle, IonIcon, IonButtons, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { heart } from 'ionicons/icons';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss'],
   standalone: true,
-  imports: [IonToolbar,IonTitle]
+  imports: [IonToolbar,IonTitle, IonIcon, IonButtons, IonButton]
 })
 export class TopbarComponent  implements OnInit {
 
-  @Input() title?: string;
+  @Input() title?: string = "pokedex";
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      heart: heart
+    });
+  }
 
   ngOnInit() {}
 
