97dbbc48cbbe9fec0af02ae35e79394a0ee3091c
Caio Orleans <caioorleansdm@gmail.com>
2025-06-18 12:50:17 -0300

    feat: recebendo nome do pokemon pela rota

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -2,19 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { TopbarComponent } from "../../components/topbar/topbar.component";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TopbarComponent]
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  pokemonName?: string;
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.pokemonName = this.route.snapshot.paramMap.get('name') || undefined;
   }
 
 }
